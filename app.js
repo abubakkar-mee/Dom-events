bd4e995924f84c2a3419eeb5befb409a847a1c2c
Abu Bakkar Siddique <abubakkar.mee@gmail.com>
2022-12-19 23:12:45 +0600

    commit

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const button = document.getElementById("btn")
-const file = document.getElementById("file")
-const click = document.getElementById("click")
+const button = document.getElementById("btn");
+const file = document.getElementById("file");
+const click = document.getElementById("click");
+const close = document.getElementById("close");
+const color_input = document.getElementById("color-input");
+const color = document.getElementById("color");
 
-button.addEventListener("click", ()=>{
-    file.click()
-})
+button.addEventListener("click", () => {
+  file.click();
+});
 
+click.addEventListener("click", (e) => {
+  const value = prompt("Submit your name");
+  const el = document.createElement("h1");
+  el.innerHTML = value;
+  document.body.append(el);
+});
 
-click.addEventListener("click", (e)=>{
-    const value =  prompt("Submit your name")
-    const el = document.createElement("h1");
-    el.innerHTML = value;
-    document.body.append(el)
+close.addEventListener("click", (e) => {
+  // window.location.replace("http://www.google.com")
+  // window.history.forward()
+  alert("Happy learning");
+});
 
-})
+color.addEventListener("click", (e) => {
+  color_input.click();
+});
+
+color_input.addEventListener("input", (e) => {
+  const color_value = e.target.value;
+  document.body.style.backgroundColor = color_value;
+});
